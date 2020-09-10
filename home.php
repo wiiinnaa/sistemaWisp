5d34c97c91ce53385542466127d61a83d6c7cebd
Wilfredo <wiiinnaa@gmail.com>
2020-09-10 17:31:31 -0500

    actualizacion de redireciomiento de pagina

--- home.php
+++ home.php
@@ -51,9 +51,9 @@
                     <div class="menu list-group-flush">
                         <a href="#" class="list-group-item list-group-item-action text-muted bg-light p-3 border-0"><i class="icon ion-md-apps lead mr-2"></i> Inicio</a>
                         <a href="antena.php" class="list-group-item list-group-item-action text-muted bg-light p-3 border-0"><i class="icon ion-md-wifi lead mr-2"></i> Atenas</a>
-                        <a href="#" class="list-group-item list-group-item-action text-muted bg-light p-3 border-0"><i class="icon ion-md-contacts lead mr-2"></i> Clientes</a>
+                        <a href="cliente.php" class="list-group-item list-group-item-action text-muted bg-light p-3 border-0"><i class="icon ion-md-contacts lead mr-2"></i> Clientes</a>
                         <a href="usuario.php" class="list-group-item list-group-item-action text-muted bg-light p-3 border-0"><i class="icon ion-md-person lead mr-2"></i> Usuarios</a>
-                        <a href="tarifa" class="list-group-item list-group-item-action text-muted bg-light p-3 border-0"><i class="icon ion-md-document lead mr-2"></i> Tarifas</a>
+                        <a href="tarifa.php" class="list-group-item list-group-item-action text-muted bg-light p-3 border-0"><i class="icon ion-md-document lead mr-2"></i> Tarifas</a>
                         <a href="#" class="list-group-item list-group-item-action text-muted bg-light p-3 border-0"><i class="icon ion-md-clipboard lead mr-2"></i> Reportes</a>
                     </div>
                 </div>
